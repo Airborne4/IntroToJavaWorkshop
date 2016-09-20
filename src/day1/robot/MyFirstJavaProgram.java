227250f174f798346bcb1b56847b4fe87a33c3c6
Goldeenzu <league@WTS10.attlocal.net>
2016-09-20 16:58:01 -0700

    Java Day 2 9-20-16

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,6 +7,22 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
+		int x=2;
+		Robot bastion=new Robot();
+		for (int i = 0; i < 4; i++) {
+			
+		
+	bastion.setPenColor(0, 0, 255);
+			bastion.penDown();
+			bastion.sparkle();
+		bastion.setSpeed(15);
+		bastion.move(500);
+		bastion.turn(90);
+		
+	
+		}
+		
+		
 
 	
 		
